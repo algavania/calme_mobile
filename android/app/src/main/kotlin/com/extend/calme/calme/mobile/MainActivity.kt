989e146c9d76b60c887a45f721072435da1c5c95
Alga <vaniaalga@gmail.com>
2024-10-31 08:22:06 +0700

    chore: init project

---
+++ android/app/src/main/kotlin/com/extend/calme/calme/mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.extend.calme.calme_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:calme_mobile/app/app.dart';
+import 'package:calme_mobile/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
